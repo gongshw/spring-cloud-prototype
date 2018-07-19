9af24d84c71e6ba40af6b27c0fd838a367b9e88f
Shiwei Gong <gongshw1992@gmail.com>
2018-07-19 19:18:46 +0800

    add Dockerfile

--- build.module.sh
+++ build.module.sh
@@ -2,7 +2,8 @@
 
 echo starting build $1
 
-jar_file=".${1//://}/build/libs/*"
+module_path=".${1//://}"
+jar_file="${module_path}/build/libs/*"
 out_bin="output/bin"
 
 bash gradlew $1:bootJar || exit 1
@@ -10,9 +11,9 @@ bash gradlew $1:bootJar || exit 1
 
 mkdir -p ${out_bin}
 
-jar_file=".${1//://}/build/libs/*"
 echo copying ${jar_file} to ${out_bin}
 cp ${jar_file} ${out_bin}
 
+bash gradlew $1:docker || echo "unable to create docker image. it's ok."
 
 echo $1 build done!
